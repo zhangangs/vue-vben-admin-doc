e4e60189cf21801b00e7f60c317ff1abfa97976b
jinmao88 <jinmao88@users.noreply.github.com>
2024-03-25 03:43:21 +0000

    deploy: 523095e76289e499d953bb67f47889eecca3762c

---
+++ assets/README.md.37c37e4a.lean.js
@@ -0,0 +1,1 @@
+import{o as n,c as a,a as e}from"./app.8cddb23b.js";const s='{"title":"vue-vben-admin-doc","description":"","frontmatter":{},"headers":[{"level":2,"title":"如何本地开发","slug":"如何本地开发"}],"relativePath":"README.md","lastUpdated":1711338181310}',t={},o=e('',3);t.render=function(e,s,t,c,d,i){return n(),a("div",null,[o])};export default t;export{s as __pageData};
---
+++ assets/other_server.md.47d25430.lean.js
@@ -0,0 +1,1 @@
+import{o as e,c as a,a as n}from"./app.8cddb23b.js";const s='{"title":"测试服务器","description":"","frontmatter":{},"headers":[{"level":2,"title":"使用","slug":"使用"}],"relativePath":"other/server.md","lastUpdated":1711338181314}',t={},r=n('',5);t.render=function(n,s,t,o,c,d){return e(),a("div",null,[r])};export default t;export{s as __pageData};
